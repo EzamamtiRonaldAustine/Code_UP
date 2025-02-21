d01f21ed03118a993d6d668d54bb9822016d6da4
Your Name <145444276+EzamamtiRonaldAustine@users.noreply.github.com>
2025-02-21 08:17:03 +0300

    js

--- hello_server/index.js
+++ hello_server/index.js
@@ -4,6 +4,12 @@ const app = express();
 
 const port = 8080;
 
+function requestLogger(request, response, next){
+    console.log(`Request Method: ${request.method}, URL: ${request.url}`);
+    next();
+}
+
+app.use(requestLogger);
 app.get(`/`, (req, res)=>{
     res.send(`Hello world uso `);
 })
