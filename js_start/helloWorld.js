c1ab4cb158ee684b11d0290604b61269a2522249
Your Name <145444276+EzamamtiRonaldAustine@users.noreply.github.com>
2025-02-23 22:21:46 +0300

    sato js

---
+++ js_start/helloWorld.js
@@ -0,0 +1,72 @@
+console.log("Hello World");
+
+// Variables
+// declare variable num 
+let num; 
+ 
+// assign 5 to num 
+num = 5; 
+
+// declare variable num1 and assign 5 to it 
+//let num1 = 5; 
+ 
+// declare variable num2 and assign 6 to it 
+//let num2 = 6; 
+
+// declare variables num1, num2, and num3 
+// assign values 5, 6, and 7 respectively 
+
+let num1 = 5, num2 = 6, num3 = 7; 
+
+// assign 5 to variable score 
+let score = 5;  
+console.log(score); // 5 
+ 
+// change the value of score to 3 
+score = 3;  
+console.log(score); // 3 
+
+
+let age = 23; 
+let Age = 20; 
+console.log(age); // 23 
+console.log(Age); // 20 
+
+
+// assign 5 to num 
+const num4 = 5; 
+ 
+// assign 10 to num 
+// num4 = 10;   
+// console.log(num4) // Error! constant cannot be changed
+
+// Error! Missing initializer in const declaration 
+// const x; 
+ 
+// // attempt to initialize constant after declaration 
+// x = 5; 
+ 
+// console.log(x) 
+
+
+function calculateProduct(num1, num2) {
+    return num1 * num2; // Return the product of num1 and num2
+}
+
+const result = calculateProduct(5, 3);
+console.log(result); // Output: 15
+
+
+let message = "Hello, JavaScript!"; 
+console.log(message); 
+ 
+// Output: Hello, JavaScript! 
+
+console.log("Good Morning!"); 
+console.log(2000); 
+
+// store value in greet variable 
+const greet = "Hello"; 
+ 
+// print the value of greet variable 
+console.log(greet);
---
+++ js_start/tempCodeRunnerFile.js
@@ -0,0 +1,44 @@
+// Use a while loop to find the sum of numbers from 1 to 100
+// let sum = 0;
+// let count = 1;
+
+// while (count <= 100) {
+//     sum += count; // Add the current count to sum
+//     count++; // Increment count
+// }
+
+// console.log("Sum of numbers from 1 to 100 is:", sum);
+
+// // Keep asking for a number between 1 and 10 using a do-while loop
+// let userInput;
+// do {
+//     userInput = prompt("Enter a number between 1 and 10:");
+//     userInput = parseInt(userInput); // Convert input to an integer
+// } while (userInput < 1 || userInput > 10);
+
+// console.log("Valid number entered:", userInput);
+
+// // Print numbers from 1 to 10, but stop when it reaches 7
+// for (let i = 1; i <= 10; i++) {
+//     if (i === 7) {
+//         break; // Exit the loop when i is 7
+//     }
+//     console.log(i);
+// }
+
+// // Print numbers from 1 to 10, but stop when it reaches 7
+// for (let i = 1; i <= 10; i++) {
+//     if (i === 7) {
+//         break; // Exit the loop when i is 7
+//     }
+//     console.log(i);
+// }
+
+// // Use nested loops to print a right-angled triangle pattern
+// for (let i = 1; i <= 5; i++) {
+//     let row = '';
+//     for (let j = 1; j <= i; j++) {
+//         row += '*'; // Append '*' to the row
+//     }
+//     console.log(row); // Print the current row
+// }
