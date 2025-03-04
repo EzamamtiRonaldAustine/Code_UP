254d5ff91f4df0904a7fcd43c7f962fa1a452061
Your Name <145444276+EzamamtiRonaldAustine@users.noreply.github.com>
2025-03-04 23:16:37 +0300

    js

--- mi_react/t-t-t/src/App.js
+++ mi_react/t-t-t/src/App.js
@@ -1,7 +1,16 @@
+import { useState} from 'react';
 import './App.css';
-//component
-function Square({value}) {
-  return <button className="square">1</button>;
+//component should be in capital letter
+function Square() {
+  const [name, setName] = useState(null);
+
+  function handleButtonClick(){
+    setName("X")
+    console.log("Click action", name);
+
+  }
+
+  return <button onClick={handleButtonClick} className="square">{name}</button>;
 }
 function App() {
   return (
