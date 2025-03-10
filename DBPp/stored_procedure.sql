29a698e32be946a5b6c1e95bf07e3dbdb8722f3f
Your Name <145444276+EzamamtiRonaldAustine@users.noreply.github.com>
2025-03-10 10:32:43 +0300

    sql

---
+++ DBPp/stored_procedure.sql
@@ -0,0 +1,9 @@
+-- Stored procedure: is a prepared SQL code that can be saved/stored  within the DBMS
+-- and reused when needed
+-- syntax
+-- Create procedure name(_,_,_)
+-- Begin
+-- select * from employee where job = ID
+-- end/ or end;
+
+-- / @ delimiter /
