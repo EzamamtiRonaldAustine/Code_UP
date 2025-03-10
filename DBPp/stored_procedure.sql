9ce1ac149d5127f202464ea77b5dc0235cb72ba5
Your Name <145444276+EzamamtiRonaldAustine@users.noreply.github.com>
2025-03-10 10:57:49 +0300

    procedures

--- DBPp/stored_procedure.sql
+++ DBPp/stored_procedure.sql
@@ -1,3 +1,4 @@
+-- Active: 1738051755016@@127.0.0.1@3306@bscs
 -- Stored procedure: is a prepared SQL code that can be saved/stored  within the DBMS
 -- and reused when needed
 -- syntax
@@ -7,3 +8,45 @@
 -- end/ or end;
 
 -- / @ delimiter /
+
+
+DELIMITER /
+
+CREATE PROCEDURE teachers()
+
+BEGIN
+SELECT * FROM employee WHERE job = 'teacher';
+
+end/
+
+DROP PROCEDURE teachers;
+
+CALL teachers()/
+
+CREATE PROCEDURE jobtype(in id1 VARCHAR(20))
+BEGIN
+SELECT * FROM employee WHERE job = id1;
+
+end/
+
+CALL jobtype('manager')/
+
+CALL jobtype('clerk')/
+
+CALL jobtype('teacher')/
+
+SELECT * FROM employee;
+
+UPDATE employee SET job = "Teacher" WHERE EmpNo = 'E003';
+
+CREATE PROCEDURE jobsum(in id2 VARCHAR(20))
+BEGIN
+SELECT SUM(salary) AS Total_amount FROM employee WHERE job = id2;
+
+end/
+
+DROP PROCEDURE jobsum;
+
+CALL jobsum('manager')/
+
+
