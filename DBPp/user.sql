7d6716c992aff876d3d475d43f0b0f0d9bce228f
Your Name <145444276+EzamamtiRonaldAustine@users.noreply.github.com>
2025-03-17 12:44:46 +0300

    bd

--- DBPp/user.sql
+++ DBPp/user.sql
@@ -2,6 +2,8 @@ CREATE USER 'user1'@'localhost' IDENTIFIED BY 'password1';
 
 ALTER USER 'user1'@'localhost' password EXPIRE;
 
+select user, host from mysql.user;
+
 ALTER USER 'user1'@'localhost' ACCOUNT LOCK;
 
 ALTER USER 'user1'@'localhost' ACCOUNT UNLOCK;
