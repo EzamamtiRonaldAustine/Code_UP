c0ee624b7aae9ee89b7222e65c024cf68170c7b3
Your Name <145444276+EzamamtiRonaldAustine@users.noreply.github.com>
2025-09-07 20:30:20 +0300

    greeting script

---
+++ CLI_learning/greet.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Simple greeting script
+
+# Asking for the user's name
+
+echo -n "Enter your name:"
+read name
+
+# Print a greeting 
+echo "Hello, $name! Welcome to Linux scripting lol"
